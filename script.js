db6f6010118ea7cb6b30a348cee57174805e4a14
Rashed Abdullah <maarashed40@gamil.com>
2023-09-17 23:18:29 +0600

    somthing added

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ function bukhari1(){
     let bukhariPage = document.querySelector(`#bukhariPage`);
     bukhariPage.innerHTML = `সহীহুল বুখারী হাদীস জগতের সবচেযে প্রসিদ্ধ ও সহীহ কিতাব। তাই নতুন করে এর মূলায়্যণ করার কিছু নাই।`
 }
+
+// deleted 'update2' in github;
